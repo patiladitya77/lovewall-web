48c48bcdee9bcb003df0a09821a63981c7abb420
patiladitya77 <adpatil587@gmail.com>
2025-07-29 20:26:13 +0530

    saving user to mongodb once signedUp

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,39 @@
+"use client";
 import Navbar from "@/components/Navbar";
 import OverView from "@/components/OverView";
 import Spaces from "@/components/Spaces";
+import { useUser } from "@clerk/nextjs";
+import axios from "axios";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { user, isSignedIn } = useUser();
+
+  useEffect(() => {
+    if (!user) return;
+
+    const { fullName, imageUrl, primaryEmailAddress } = user;
+
+    const saveUser = async () => {
+      try {
+        const res = await axios.post(
+          process.env.NEXT_PUBLIC_API_BASE_URL + "api/auth/saveuser",
+          {
+            name: fullName,
+            photoURL: imageUrl,
+            email: primaryEmailAddress?.emailAddress,
+          },
+          { withCredentials: true }
+        );
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    saveUser();
+  }, [user]);
+
   return (
     <div>
       <div>
