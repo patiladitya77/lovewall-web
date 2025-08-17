e49d0c4335de4219bd9ee610574f1df287024245
patiladitya77 <adpatil587@gmail.com>
2025-08-18 00:56:24 +0530

    Feat: Delete testimonial

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import VideoPreview from "./VideoPreview";
+import axios from "axios";
+import { useAuth } from "@clerk/nextjs";
 type Testimonial = {
   feedbackType: string;
   feedback: string;
@@ -8,6 +10,7 @@ type Testimonial = {
   videoUrl: string;
   spaceId: string;
   starRating: number;
+  _id: string;
 };
 
 type TestimonialCardProps = {
@@ -17,7 +20,39 @@ type TestimonialCardProps = {
 const TestimonialCard = ({ testimonials }: TestimonialCardProps) => {
   console.log(testimonials);
   const [isExpanded, setIsExpanded] = useState(false);
-  const { feedbackType, feedback, name, senderEmail, videoUrl } = testimonials;
+  const [confirmDelete, setConfirmDelete] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [err, setError] = useState("");
+  const { feedbackType, feedback, name, senderEmail, videoUrl, _id } =
+    testimonials;
+  const { getToken } = useAuth();
+  const handleDeleteTestimonial = async () => {
+    setLoading(true);
+    try {
+      const token = await getToken();
+      const res = await axios.delete(
+        process.env.NEXT_PUBLIC_API_BASE_URL +
+          "api/testimonial/deleteTestimonial/" +
+          _id,
+
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (res.status === 200) {
+        setConfirmDelete(false);
+      } else {
+        setError("Error while deleting, try again");
+      }
+      console.log(res);
+    } catch (error) {
+      console.log("ERROR: " + error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className="bg-base-100 rounded-md p-4 my-4 ">
       <div className="flex justify-between">
@@ -86,26 +121,59 @@ const TestimonialCard = ({ testimonials }: TestimonialCardProps) => {
         </div>
       </div>
       {isExpanded && (
-        <div className="flex justify-end">
-          {/* <button>delete</button> */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-4 my-2 mx-1"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-            />
-          </svg>
+        <>
+          <div className="flex justify-end">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-4 my-2 mx-1"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+              />
+            </svg>
 
-          <button className="text-gray-200 text-md">Delete</button>
-        </div>
+            <button
+              className="text-gray-200 text-md cursor-pointer"
+              onClick={() => setConfirmDelete(true)}
+            >
+              Delete
+            </button>
+          </div>
+
+          {/* Confirmation card */}
+          {confirmDelete && (
+            <div className="fixed inset-0 flex items-center justify-center bg-black/30 bg-opacity-50 z-50">
+              <div className="card w-96 bg-base-100 card-md shadow-sm">
+                <div className="card-body">
+                  <h2 className="card-title">Are you sure?</h2>
+                  <p>This action will permanently delete this testimonial.</p>
+                  <div className="justify-end card-actions">
+                    <button
+                      className="btn btn-error"
+                      onClick={handleDeleteTestimonial}
+                    >
+                      {loading ? "Deleting..." : "Confirm"}
+                    </button>
+                    <button
+                      className="btn"
+                      onClick={() => setConfirmDelete(false)}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </>
       )}
+
       <div className="flex justify-end">
         {isExpanded ? (
           <svg
